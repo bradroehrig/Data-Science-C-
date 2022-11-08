a225c940e110e16195e76c79d453496ee57b6c13
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-11-07 18:48:00 -0600

    Create work.cpp

---
+++ work.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int i = 0;
+
+    while (i <= 10)
+    {
+        std::cout << i << std::endl;
+        i++;
+    }
+    
+    std::cout << "All Finished!" << std::endl;
+
+    return 0;
+
+}
