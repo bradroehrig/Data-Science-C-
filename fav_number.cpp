9053da96052a583dd1bbd4bf76294f79a46c415d
bradroehrig <63978952+bradroehrig@users.noreply.github.com>
2022-11-08 17:25:21 -0600

    Update fav_number.cpp

--- fav_number.cpp
+++ fav_number.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 int main(){
+    
+    int favorite_number;
     std::cout << "Enter your favorite number between 1 to 100.";
 
+    std::cin >> favorite_number;
     return 0;
 }
