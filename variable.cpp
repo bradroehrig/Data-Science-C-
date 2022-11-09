cd55866ba8e75791b4b7db865e65af1f2980fd0b
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-11-08 21:02:11 -0600

    Create variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,18 @@
+//Section 5
+// Basic I/O using cin and cout
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int num1;
+int num2;
+double num3;
+
+cout << "Enter an integer";
+cin >> num1;
+cout << "You enter: " << num1 << endl;
+
+}
