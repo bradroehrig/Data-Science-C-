6b7b59114528cad47e616c91762099fa3d40c7d8
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-11-14 17:34:25 -0600

    Create cleaning.cpp

---
+++ cleaning.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int main(){
+    
+    using namespace std;
+
+    int number_of_rooms;
+
+    
+
+
+
+
+
+
+
+
+
+    int favorite_number;
+    cout << "Enter your favorite number between 1 to 100.";
+
+    cin >> favorite_number;
+
+    cout << "Amazing!! That is my favorite number too!" << endl;
+    return 0;
+}
