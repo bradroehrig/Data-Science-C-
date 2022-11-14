de8357d58a21bb316af3906cef7023deb313479d
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-11-14 17:45:18 -0600

    Update cleaning.cpp

--- cleaning.cpp
+++ cleaning.cpp
@@ -4,8 +4,14 @@ int main(){
     using namespace std;
 
     int number_of_rooms;
+    cout << "Enter the number of rooms to clean: ";
+    cin >> number_of_rooms;
 
-    
+    int price_per_room {30};
+
+    cout << "Number of rooms: "<< number_of_rooms;
+    cout << "Price per room: " << price_per_room;    
+    cout << "$" << number_of_rooms * price_per_room;
 
 
 
@@ -14,12 +20,11 @@ int main(){
 
 
 
+    // int favorite_number;
+    // cout << "Enter your favorite number between 1 to 100.";
 
-    int favorite_number;
-    cout << "Enter your favorite number between 1 to 100.";
+    // cin >> favorite_number;
 
-    cin >> favorite_number;
-
-    cout << "Amazing!! That is my favorite number too!" << endl;
+    // cout << "Amazing!! That is my favorite number too!" << endl;
     return 0;
 }
