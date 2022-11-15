600d72a9edf4b625f0416ebb201616e376427c4a
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-11-15 17:43:00 -0600

    Create euros.cpp

---
+++ euros.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    const double usd_per_eur {1.19};
+    cout << "Welcome to the EUR to USD converter" << endl;
+    
+
+
+    cout << endl;
+
+    return 0;
+}
