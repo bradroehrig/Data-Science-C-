78d43c41bebbce426f729aca262afee05f657bbd
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-11-14 19:22:44 -0600

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+int main(){
+    
+    using namespace std;
+
+
+
+
+
+
+
+
+    // int number_of_rooms {0};
+    // cout << "Enter the number of rooms to clean: ";
+    // cin >> number_of_rooms;
+
+    // const double price_per_room {30};
+    // const double sales_tax {0.06};
+    // const double quote_length {30}; //days
+
+    // cout << "\nEstimate for carpet cleaning service" << endl;
+    // cout << "Number of rooms: "<< number_of_rooms <<endl; 
+    // cout << "Price per room: $" << price_per_room <<endl;   
+    // cout << "Cost: $" << number_of_rooms * price_per_room <<endl;
+    // cout << "Tax: $" << number_of_rooms * price_per_room * sales_tax <<endl;
+    // cout << "=============================================" <<endl;
+    // cout << "Total estimate is $" << (number_of_rooms * price_per_room) + (number_of_rooms * price_per_room * sales_tax) <<endl; 
+    // cout << "This estimate is good for " << quote_length << " days" << endl;
+
+    return 0;
+}
