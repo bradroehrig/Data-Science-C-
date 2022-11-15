a531218f897ed5fa4d7951c7bacea47b9aadddd4
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-11-15 17:08:30 -0600

    Create statement.cpp

---
+++ statement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+    int num1 {10};
+    int num2 {20};
+
+    cout << "num1 is " << num1 << endl;
+    cout << "num2 is " << num2 << endl;
+
+    cout << endl;
+
+    return 0;
+}
