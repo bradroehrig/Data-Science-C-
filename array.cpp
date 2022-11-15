e6809e9f6694b1e0a8df341cf964b565510854ca
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-11-14 21:16:38 -0600

    main

--- array.cpp
+++ array.cpp
@@ -53,5 +53,25 @@ int main(){
 
     cout << "\nThere are now " << test_scores.size() << " scores in the vector" << endl;
 
+    vector <vector<int>> movie_ratings
+    {
+        {1,2,3,4},
+        {1,2,3,4},
+        {1,2,3,4}
+    };
+    
+    cout << "\nHere are the movie rating for reviewer #1 using array syntax:" <<endl;
+    cout << movie_ratings[0][0] << endl;
+    cout << movie_ratings[0][1] << endl;
+    cout << movie_ratings[0][2] << endl;
+    cout << movie_ratings[0][3] << endl;
+
+    cout << "\nHere are the movie rating for reviewer #1 using vector syntax:" <<endl;
+    cout << movie_ratings.at(0).at(0)<< endl;
+    cout << movie_ratings.at(0).at(1)<< endl;
+    cout << movie_ratings.at(0).at(2)<< endl;
+    cout << movie_ratings.at(0).at(3)<< endl;
+
+    cout << endl;
     return 0;
 }
