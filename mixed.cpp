c3530f05526df0466ed393f62b03aae8cdb58878
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-11-15 19:09:22 -0600

    Update mixed.cpp

--- mixed.cpp
+++ mixed.cpp
@@ -5,5 +5,23 @@ using namespace std;
 int main(){
 
 
+    int total {};
+    int num1 {}, num2{}, num3{};
+    const int count {3};
+
+    cout << "enter 3 integers separated by spaces:";
+    cin >> num1 >> num2 >> num3;
+
+    total = num1 + num2 + num3;
+
+    double average {0.0};
+
+    average= total / count;
+
+    cout << "Ther 3 numbers were: " << num1 << ", " << num2 << ", " << num3 << endl;
+    cout << "The sum of the numbers is: " << total << endl;
+    cout << "Ther average of the numbers is: " << average << endl;
+
+    cout <<endl;
     return 0;
 }
