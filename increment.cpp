9ba233d31316a4ac5e8d3db43237efc7004fbc3a
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-11-15 18:53:37 -0600

    Update increment.cpp

--- increment.cpp
+++ increment.cpp
@@ -7,5 +7,44 @@ int main(){
     int counter {10};
     int result {0};
 
+
+    cout << "Counter:" << counter << endl;
+
+    result = counter++ + 10;
+    cout << "Counter:" << counter << endl;
+    cout << "Result:" << result << endl;
+
+    //==========================================
+
+    result = ++counter + 10;
+    cout << "Counter:" << counter << endl;
+    cout << "Result:" << result << endl;
+
+    //==========================================
+
+    result = counter++;
+    cout << "Counter:" << counter << endl;
+    cout << "Result:" << result << endl;
+
+    //==========================================
+
+    result = ++counter;
+    cout << "Counter:" << counter << endl;
+    cout << "Result:" << result << endl;
+
+    //==========================================
+
+    counter = counter + 1;
+    cout << "Counter:" << counter << endl;
+
+    counter++;
+    cout << "Counter:" << counter << endl;
+
+    ++counter;
+    cout << "Counter:" << counter << endl;
+    
+    --counter;
+    cout << "Counter:" << counter << endl;
+
     return 0;
 }
