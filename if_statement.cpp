d159a2ad654287c61ab6407da58450bcce928cbb
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-11-16 19:58:05 -0600

    Create if_statement.cpp

---
+++ if_statement.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num{};
+    const int min {10};
+    const int max {100};
+
+    cout << "Enter a number between " << min << " and " << max << ":";
+    cin >> num;
+
+
+
+    cout << endl;
+    return 0;
+}
+
